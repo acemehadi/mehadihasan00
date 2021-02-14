7f1f63422e365c09a67b0438373822addc609edb
mehadihasan00 <meahdihasan.rajib10@gmail.com>
2021-02-14 21:59:04 +0600

    Add Total Addition number

---
+++ Helllow.py
@@ -0,0 +1,4 @@
+number_vaule=10
+number_vaule2=20
+addition = number_vaule+number_vaule2
+print(f"Total : {addition}")
--- Master.py
+++
@@ -1,3 +0,0 @@
-
-username=input("Enter your Name :  ")
-print(f"Hellow {username}")
